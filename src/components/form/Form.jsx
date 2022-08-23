85219e26c5bad0491ddb95467e562a3126ad1df7
ajarek <ajarek@poczta.onet.pl>
2022-08-24 00:26:13 +0200

    add name validate

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -1,66 +1,94 @@
 import "./form.css";
-import { Label } from "../../components/label/Label";
-import { Input } from "../../components/input/Input";
-import { Textarea } from "../../components/textarea/Textarea";
-import { Button } from "../../components/button/Button";
-export function Form() {
-  return (
-    <div className="form">
-      <p className="title">Contact Form</p>
-      <div className="form-row">
-        <div className="input-wrapper">
+import React from "react";
+import { Label } from "../../components/label/Label"
+import { Input } from "../../components/input/Input"
+import { Textarea } from "../../components/textarea/Textarea"
+import { Button } from "../../components/button/Button"
+export class Form extends React.Component {
+  state = {
+    inputValueName: "",
+    validateName:"",
+  }
+  InputName = (e) => {
+    this.setState({ inputValueName: e.target.value })
+    setTimeout(() => {
+      if (
+        this.state.inputValueName.match(
+          /^([A-Z][a-z]{2,})$/
+        )
+      ) {
+        this.setState({ validateName: "Valid" });
+      }
+      else {
+        this.setState({
+          validateName: "Invalid:Only first capital letter, min = 3, no digit",
+        });
+      }
+    },0)
+  }
+  render() {
+    return (
+      <form className="form">
+        <p className="title">Contact Form</p>
+        <div className="form-row">
+          <div className="input-wrapper">
+            <div className="form-wrapper">
+              <Label id={"name"} label={"Your Name"} />
+              <Input
+                onChange={this.InputName}
+                value={this.state.inputValueName}
+                id={"name"}
+                type={"text"}
+                name={"name"}
+                placeholder={"👤 John"}
+              />
+            </div>
+            <p>{this.state.validateName}</p>
+            <div className="form-wrapper">
+              <Label id={"mail"} label={"Mail"} />
+              <Input
+                id={"mail"}
+                type={"email"}
+                name={"mail"}
+                placeholder={"✉️ john@doe.en "}
+              />
+            </div>
+            <div className="form-wrapper">
+              <Label id={"phone"} label={"Phone"} />
+              <Input
+                id={"phone"}
+                type={"phone"}
+                name={"phone"}
+                placeholder={"📞 +48 573 219 230"}
+              />
+            </div>
+          </div>
           <div className="form-wrapper">
-            <Label id={"name"} label={"Your Name"} />
-            <Input
+            <Label
               id={"name"}
-              type={"text"}
-              name={"name"}
-              placeholder={"👤 John Doe"}
+              label={"Message"}
+              style={{ paddingLeft: "1rem" }}
             />
-          </div>
-          <div className="form-wrapper">
-            <Label id={"mail"} label={"Mail"} />
-            <Input
-              id={"mail"}
-              type={"email"}
-              name={"mail"}
-              placeholder={"✉️ john@doe.en "}
-            />
-          </div>
-          <div className="form-wrapper">
-            <Label id={"phone"} label={"Phone"} />
-            <Input
-              id={"phone"}
-              type={"phone"}
-              name={"phone"}
-              placeholder={"📞 +48 573 219 230"}
-            />
+            <Textarea cols={"20"} rows={"8"} />
           </div>
         </div>
         <div className="form-wrapper">
-          <Label id={"name"}
-           label={"Message"} 
-           style={{paddingLeft:'1rem'}}
-           />
-          <Textarea cols={"20"} rows={"8"} />
+          <Label id={"name"} label={"Services"} />
         </div>
-      </div>
-      <div className="form-wrapper">
-        <Label id={"name"} label={"Services"} />
-      </div>
-      <div className="form-button">
-        <Button label={"Web Design"} />
-        <Button label={"Web Design"} />
-        <Button label={"Web Design"} />
-        <Button label={"Web Design"} />
-      </div>
-      <div className="form-message">
-        <Button
-          label={"Send Message"}
-          style={{ background: "#fa949d", color: "#fff" }}
-        />
-      </div>
-    </div>
-  );
+        <div className="form-button">
+          <Button label={"Web Design"} />
+          <Button label={"Web Design"} />
+          <Button label={"Web Design"} />
+          <Button label={"Web Design"} />
+        </div>
+        <div className="form-message">
+          <Button
+            label={"Send Message"}
+            style={{ background: "#fa949d", color: "#fff" }}
+          />
+        </div>
+      </form>
+    )
+  }
 }
-export default Form;
+export default Form
--- src/components/input/Input.jsx
+++ src/components/input/Input.jsx
@@ -2,7 +2,14 @@ import "./input.css";
 export function Input(props) {
   return (
     <div className="input-container">
-      <input type={props.type} name={props.name} id={props.id} placeholder={props.placeholder} />
+      <input
+        onChange={props.onChange}
+        value={props.value}
+        type={props.type}
+        name={props.name}
+        id={props.id}
+        placeholder={props.placeholder}
+      />
     </div>
   );
 }
